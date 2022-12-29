df200c8abf641c424e03033afd27ccf5212dcf46
dev-sumukha <sumukhasureban@gmail.com>
2022-12-29 21:13:40 +0530

    simple navigation completed

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import About from './About';
+import './App.css';
+import Contact from './Contact';
+import Home from './Home';
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/about" element={<About/>} />
+          <Route path="/contact" element={<Contact/>} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Header.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+const Header = () => {
+  return (
+    <>
+        {/* <div className="navbar">
+            <div className="logo">
+                <h1>LOGO</h1>
+            </div>
+            <div className="nav-list">
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
+            <div className="buttons">
+                <button id='signup'>SignUp</button>
+                <button id='login'>Login</button>
+            </div>
+        </div> */}
+
+        <header>
+            <a href="#">LOGO</a>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/about">About</Link>
+                    </li>
+                    <li>
+                        <Link to="/contact">Contact</Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    </>
+  )
+}
+
+export default Header
